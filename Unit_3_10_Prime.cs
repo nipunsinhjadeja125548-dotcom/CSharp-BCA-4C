67272359e987d07f3121b430743704ed73926da9
nipunsinhjadeja125548-dotcom <nipunsinh.jadeja125548@marwadiuniversity.ac.in>
2026-01-27 09:45:55 +0530

    Add files via upload

---
+++ Unit_3_10_Prime.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit_3_10_Prime
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int num,i;
+            bool isPrime = true;
+
+            Console.Write("Enter a number: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            if (num <= 1)
+            {
+                isPrime = false;
+            }
+            else
+            {
+                for (i = 2; i <= num / 2; i++)
+                {
+                    if (num % i == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+            }
+
+            if (isPrime)
+                Console.WriteLine("The number is Prime");
+            else
+                Console.WriteLine("The number is Not Prime");
+        }
+    }
+}
