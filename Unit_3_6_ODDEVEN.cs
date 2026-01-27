8da2f473c427dda03deeb9ebbe46d45e45b8c907
nipunsinhjadeja125548-dotcom <nipunsinh.jadeja125548@marwadiuniversity.ac.in>
2026-01-27 09:44:33 +0530

    Add files via upload

---
+++ Unit_3_6_ODDEVEN.cs
@@ -0,0 +1,26 @@
+﻿// even and or 
+using System;
+
+
+namespace Unit_3_6_ODDEVEN
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            Console.Write("Enter a number: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("number is Even");
+            }
+            else
+            {
+                Console.WriteLine("number is Odd");
+            }
+        }
+    }
+}
