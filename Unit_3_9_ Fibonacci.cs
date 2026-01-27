cae5833b15c3734b6db94c41a45a50489e903089
nipunsinhjadeja125548-dotcom <nipunsinh.jadeja125548@marwadiuniversity.ac.in>
2026-01-27 09:45:35 +0530

    Add files via upload

---
+++ Unit_3_9_ Fibonacci.cs
@@ -0,0 +1,31 @@
+﻿// Fibonacci series up to a number inputted by user
+
+
+using System;
+
+namespace Unit_3_9__Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 0, b = 1, c, i;
+
+            Console.Write("Enter number : ");
+            i = Convert.ToInt32(Console.ReadLine());
+
+            while (a <= i)
+            {
+                Console.Write(a + " ");
+               
+                c = a + b;
+                a = b;
+                b = c;
+            }
+        }
+
+
+
+    }
+}
